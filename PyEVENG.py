48f0d0e2df6ca2d34f526abb9a891ef89f0fdd56
Dylan-Hamel <contact@dylan-hamel.ch>
2019-03-27 20:09:42 +0100

    Insert Cumulus Network class for backup

--- PyEVENG.py
+++ PyEVENG.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # Default value used for exit()
-
-
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
+
+try:
+    import cumulus_device
+except ImportError as importError:
+    print("Error import cumulus_device")
+    print(importError)
+    exit(EXIT_FAILURE)
 
 try:
     import paramiko
@@ -52,7 +57,7 @@ class PyEVENG:
     # Getters (project, labs, node, config, ...)
     # Using REST API only
     
-    def getBackupConfig(self, project_name: str(), nodeID: str()) -> str():
+    def getBackupConfig(self, path:str(), project_name: str(), nodeID: str()):
         """
         This function will return a string that defines device image
 
@@ -67,248 +72,15 @@ class PyEVENG:
         nodeImage = nodeImage.upper()
     
         if "CUMULUS" in nodeImage :
-            return self.getCumulusNodeConfigFilesByProjectIDAndNodeID(project_name, nodeID)
-
-    def add_comment(self, comment:str()) -> str():
-        """
-        This function will return a comment to inser in config 
-        Inspire from Oxidized
+            self.getCumulusBackup(path, project_name, nodeID)
 
 
-        Args:
-            param1 (str): Comment.
-        
-        Returns:
-            str: Comment with header
-        """
+    def getCumulusBackup(self, path, project_name, nodeID):
+        cumulus = cumulus_device.CumulusDevice(
+            self._ipAddress, self._root, self._password, path, 
+            self._pod, self.getLabID(project_name), nodeID)
 
-        return "\n####### " + comment + " ######\n"
-
-    def getCumulusNodeConfigFilesByProjectIDAndNodeID(self, project_name: str(), nodeID: str()) -> str():
-        """
-        This function will return a list that contains node configuration according to project_name and name_uid given in parameter
-        This function only works with Cumulus Network Nodes !!
-
-        Args:
-            param1 (str): EVE-NG Project Name.
-            param2 (str): EVE-NG Node ID.
-        
-        Returns:
-            list: list of configuration files
-        """
-
-        self.check_param_type_str(project_name)
-        self.check_param_type_str(nodeID)
-
-        sshClient = paramiko.SSHClient()
-        sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        sshClient.connect(hostname=self._ipAddress,
-                        username=self._root, password=self._rootPassword)
-
-        stdin, stdout, stderr = sshClient.exec_command(
-            "ls /opt/unetlab/tmp/"+self._pod+"/"+self.getLabID(project_name)+"/"+nodeID+"/ | grep qcow2")
-        qcow2 = stdout.readlines()
-        if qcow2.__len__() is not 0:
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo modprobe nbd max_part=8")
-            stdout.readlines()
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo qemu-nbd -c /dev/nbd0 /opt/unetlab/tmp/"+self._pod+"/"+self.getLabID(project_name)+"/"+nodeID + "/virtioa.qcow2")
-            stdout.readlines()
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo partx -a /dev/nbd0")
-            stdout.readlines()
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo mkdir /mnt/disk")
-            stdout.readlines()
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo mount /dev/nbd0p4 /mnt/disk/")
-            stdout.readlines()
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo ls /mnt/disk/etc/")
-            output = stdout.readlines()
-
-            cumulusNetworkConfigurationFilesInETC = list()
-            for configFile in output:
-                cumulusNetworkConfigurationFilesInETC.append(configFile[:-1])
-
-            # Retrieve configuration files
-            config = ""
-            # HOSTNAME
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/hostname")
-            config += "\n####### -> " + (str(stdout.readline()))
-
-            # HOSTS
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/network/interfaces")
-            temp = stdout.readlines()
-
-            config += self.add_comment("HOSTS")
-            for line in temp:
-                config += str(line)
-
-            # INTERFACES
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/hosts")
-            temp = stdout.readlines()
-
-            config += self.add_comment("INTERFACES")
-            for line in temp:
-                config += str(line)
-
-            
-            # INTERFACES
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo grep -r \"\" /mnt/disk/etc/network/interface* | cut -d \"/\" -f 4-")
-            temp = stdout.readlines()
-
-            config += self.add_comment("INTERFACES - BIS")
-            for line in temp:
-                config += str(line)
-            
-            # FR Routing
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/frr/frr.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("FR Routing")
-            for line in temp:
-                config += str(line)
-
-             # Routing Daemons
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/frr/daemons")
-            temp = stdout.readlines()
-
-            config += self.add_comment("Routing Daemons")
-            for line in temp:
-                config += str(line)
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/frr/daemons.conf")
-            temp = stdout.readlines()
-            for line in temp:
-                config += str(line)
-            
-            # RESOLV.CONF
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/resolv.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("RESOLV.CONF")
-            for line in temp:
-                config += str(line)
-
-            # NTP.CONF
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/ntp.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("NTP.CONF")
-            for line in temp:
-                config += str(line)
-
-            # IP Routes
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo netstat -rn")
-            temp = stdout.readlines()
-
-            config += self.add_comment("IP Routes")
-            for line in temp:
-                config += str(line)
-            
-            # SNMP settings
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/snmp/snmpd.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("SNMP settings")
-            for line in temp:
-                config += str(line)
-
-            # MOTD
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/motd")
-            temp = stdout.readlines()
-
-            config += self.add_comment("MOTD")
-            for line in temp:
-                config += str(line)
-
-            # PASSWD
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/passwd")
-            temp = stdout.readlines()
-
-            config += self.add_comment("PASSWD")
-            for line in temp:
-                config += str(line)
-
-            # SWITCHD
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/cumulus/switchd.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("SWITCHD")
-            for line in temp:
-                config += str(line)
-
-            # PORTS
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/cumulus/ports.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("PORTS")
-            for line in temp:
-                config += str(line)
-
-            # TRAFFIC
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/cumulus/datapath/traffic.conf")
-            temp = stdout.readlines()
-
-            config += self.add_comment("TRAFFIC")
-            for line in temp:
-                config += str(line)
-
-            # ACCESS-LIST
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo iptables -L -n")
-            temp = stdout.readlines()
-
-            config += self.add_comment("ACCESS-LIST")
-            for line in temp:
-                config += str(line)
-
-            # VERSION
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cat /mnt/disk/etc/cumulus/etc.replace/os-release")
-            temp = stdout.readlines()
-
-            config += self.add_comment("VERSION")
-            for line in temp:
-                config += str(line)
-
-            # LICENCE
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo cl-license")
-            temp = stdout.readlines()
-
-            config += self.add_comment("LICENCE")
-            for line in temp:
-                config += str(line)
-
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo umount /mnt/disk")
-            stdout.readlines()
-            stdin, stdout, stderr = sshClient.exec_command(
-                "sudo qemu-nbd --disconnect /dev/nbd0")
-            o = "".join(stdout.readlines())
-            if "nbd0" not in o:
-                raise Exception("Error during nbd disconnect")
-
-            return config
-    
+        cumulus.getConfigVerbose()
 
     # ------------------------------------------------------------------------------------------
     # Getters (project, labs, node, config, ...)
