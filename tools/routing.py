b714ec5b5bdbd21f67e24a461be9a27b323dfc98
Dylan-Hamel <contact@dylan-hamel.ch>
2019-05-15 22:08:48 +0200

    New architecture for oob (NAT), update requirements

--- tools/routing.py
+++ tools/routing.py
@@ -20,36 +20,30 @@ __license__ = "MIT"
 #
 IPTABLES_INSTALL = "sudo apt-get install - y iptables"
 
-IPTABLES_DNAT = "iptables -A PREROUTING -t nat -i $(ip route show | grep $(hostname -I | awk '{{print $1}}') | awk '{{print $3}}') -p tcp --dport {} -j DNAT --to {}:{}"
-IPTABLES_ALLOWED = "iptables -A FORWARD -p tcp -d $(hostname -I | awk '{{print $1}}') --dport {} -j ACCEPT"
-IPTABLES_SNAT = "iptables -A POSTROUTING -t nat -o {} -p tcp --dport 22 -j SNAT --to {}"
+IPTABLES_DNAT = "iptables -{} PREROUTING -t nat -i $(ip route show | grep $(hostname -I | awk '{{print $1}}') | awk '{{print $3}}') -p tcp --dport {} -j DNAT --to {}:{}"
+IPTABLES_ALLOWED = "iptables -{} FORWARD -p tcp -d $(hostname -I | awk '{{print $1}}') --dport {} -j ACCEPT"
+IPTABLES_SNAT = "iptables -{} POSTROUTING -t nat -o {} -p tcp --dport 22 -j SNAT --to {}"
 
 
 #print(tools.routing.IPTABLES_DNAT.format("22022", "10.0.5.201", "22"))
 #print(tools.routing.IPTABLES_ALLOWED.format("22022"))
 #print(tools.routing.IPTABLES_SNAT.format("pnet5", "10.0.5.1"))
 
-
 # hostname -I | awk '{print $1}' = 172.16.194.239
 # ip route show | grep $(hostname -I | awk '{print $1}') | awk '{print $3}' = pnet0
-
 
 # iptables -A PREROUTING -t nat -i $(ip route show | grep $(hostname -I | awk '{print $1}') | awk '{print $3}') -p tcp --dport 22022 -j DNAT --to 10.0.5.201:22
 # iptables -A FORWARD -p tcp -d $(hostname -I | awk '{print $1}') --dport 22022 -j ACCEPT
 # iptables -A POSTROUTING -t nat -o pnet5 -p tcp --dport 22 -j SNAT --to 10.0.5.1
 
-
-
 # ####################################################################################
 # LISTE DES INTERFACES
-# ifconfig | grep encap | awk '{print $1}'
-
+# ifconfig | grep encap | awk '{print $1}' | grep -vi "vunl\|vnet"
 
 # ####################################################################################
 #
 # INFORMATIONS 
 #
-
 #iptables -A PREROUTING -t nat -i pnet0 -p tcp --dport 22022 -j DNAT --to 10.0.5.201:22
 #iptables -A FORWARD -p tcp -d 172.16.194.239 --dport 22022 -j ACCEPT
 #iptables -A POSTROUTING -t nat -o pnet5 -p tcp - dport 22 -j SNAT --to 10.0.5.1
